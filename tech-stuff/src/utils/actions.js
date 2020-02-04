966ca4d2c3e0c5396c648bd03c6cf6cc76f66a6a
Jadon Guzman <jjkolet25@tutanota.com>
2020-02-04 10:52:44 -0800

    deleted comment

--- tech-stuff/src/utils/actions.js
+++ tech-stuff/src/utils/actions.js
@@ -21,32 +21,3 @@ export const postRegInfo = data => dispatch => {
       dispatch({ type: POST_REG_FAIL, payload: err.response });
     });
 };
-
-// export const USER_LOADING = 'USER_LOADING';
-// export const USER_LOADED = 'USER_LOADED';
-// export const LOGIN_SUCC = 'LOGIN_SUCC';
-// export const LOGIN_FAIL = 'LOGIN_FAIL';
-
-// export const loadUser = () => dispatch => {
-//   dispatch({ type: USER_LOADING });
-//   axios.post(`https://use-my-tech.herokuapp.com/api/user/login`)
-//   .then(res => {
-
-//   });
-// };
-
-// export const FETCH_REG_START = 'FETCH_REG_START';
-// export const FETCH_REG_SUCC = 'FETCH_REG_SUCC';
-// export const FETCH_REG_FAIL = 'FETCH_REG_FAIL';
-
-// export const getReg = id => dispatch => {
-//   dispatch({ type: FETCH_REG_START });
-//   axiosWithAuth()
-//     .get(`/user/login`)
-//     .then(res => {
-//       dispatch({ type: FETCH_REG_SUCC, payload: res.data });
-//     })
-//     .catch(err => {
-//       dispatch({ type: FETCH_REG_FAIL, payload: err.response });
-//     });
-// };
