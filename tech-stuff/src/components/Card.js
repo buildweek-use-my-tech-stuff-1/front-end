ea4f4d2108960005d7f08f3bca02422068708a85
apple <apple@apples-MacBook-Pro-2.local>
2020-02-03 16:45:25 -0800

    periodic save 2

--- tech-stuff/src/components/Card.js
+++ tech-stuff/src/components/Card.js
@@ -1,18 +1,25 @@
 import React from "react";
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Button
+  } from 'reactstrap';
 
-const Card = () => {
+const Cards = () => {
     return (
-        <div>
-                <div className="Cards">
-                        <h2>Name</h2>
-                        <p>Name</p>
-                        <p>Description</p>
-                        <p>Price</p>
-                        <p>Avaiable ?</p>
-                        <button>Rent!</button>
-                </div>
+        <div className='test'>
+        <Card className='m-3' style={{width: '20%'}}>
+            <CardBody>
+                <CardTitle>Name</CardTitle>
+                <CardImg top width="100%" src="../Images/djm-900nxs2.png" alt="Pioneer DJM900 nexus2"/>
+                <CardText>Name</CardText>
+                <CardText>Description</CardText>
+                <CardText>Price</CardText>
+                <CardText>Avaiable ?</CardText>
+                <Button>Rent!</Button>
+            </CardBody>
+        </Card>
         </div>
     );
 };
 
-export default Card;
+export default Cards;
--- tech-stuff/src/components/OwnerDashBoard.js
+++ tech-stuff/src/components/OwnerDashBoard.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Card from './Card';
+import Cards from './Card';
 
 const OwnerDashBoard = () => {
     return (
-        <Card />
+        <Cards />
     );
 };
 
