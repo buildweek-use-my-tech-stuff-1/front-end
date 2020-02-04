ee02b1a953132001121c2a7ee5fb4fc52d0ad49a
Jadon Guzman <jjkolet25@tutanota.com>
2020-02-04 10:58:03 -0800

    up to date

--- tech-stuff/src/App.js
+++ tech-stuff/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import OwnerDashBoard from './Components/OwnerDashBoard';
-import AccountCreation from './Components/AccountCreation';
-import Login from './Components/Login';
-import { Router, Route } from 'react-router-dom';
-import PrivateRoute from './Components/PrivateRoute';
 
 const App = () => {
   return (
-    <>
-      <div>
-        <Route exact path='/' component={AccountCreation} />
-        <Route path='/login' component={Login} />
-        <PrivateRoute path='/owner-dashboard/:id' component={OwnerDashBoard} />
-      </div>
-    </>
+    <div>
+      <h1>Hello World!</h1>;
+      <OwnerDashBoard />
+    </div>
   );
 };
 
